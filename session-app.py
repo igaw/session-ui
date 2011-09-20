5878ab5b6717e32956d090402d4b8891ea024c52
Daniel Wagner <daniel.wagner@bmw-carit.de>
2011-09-20 11:47:06 +0200

    Update build instractions

--- session-app.py
+++ session-app.py
@@ -15,8 +15,8 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-# Build Instration
-# pyuic4 session-app.ui > session-app_ui.py
+# Build Instruction
+# pyuic4 session.ui > session_ui.py
 
 import sys
 from session_ui import Ui_Session
